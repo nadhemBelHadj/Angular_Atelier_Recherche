bd1186a544374a07c44acd6f375f0c3e35709a0c
Nadhem Bel Hadj <nadhemb@yahoo.com>
2021-12-07 17:19:32 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProduitsComponent } from './produits/produits.component';
 import { AddProduitComponent } from './add-produit/add-produit.component';
 import { UpdateProduitComponent } from './update-produit/update-produit.component';
+import { RechercheParCategorieComponent } from './recherche-par-categorie/recherche-par-categorie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitsComponent,
     AddProduitComponent,
-    UpdateProduitComponent
+    UpdateProduitComponent,
+    RechercheParCategorieComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recherche-par-categorie/recherche-par-categorie.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recherche-par-categorie',
+  templateUrl: './recherche-par-categorie.component.html',
+  styles: [
+  ]
+})
+export class RechercheParCategorieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
