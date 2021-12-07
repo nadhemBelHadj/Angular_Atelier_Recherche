6de90bead8546b63d000fd22c9d994f07d2a25ed
Nadhem Bel Hadj <nadhemb@yahoo.com>
2021-12-07 16:44:31 +0100

    le 07 Dec 2021 test

--- src/app/update-produit/update-produit.component.ts
+++ src/app/update-produit/update-produit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { ProduitService } from '../services/produit.service';
 import { Produit } from '../model/produit.model';
+import { Categorie } from '../model/categorie.model';
 
 @Component({
   selector: 'app-update-produit',
@@ -10,6 +11,9 @@ import { Produit } from '../model/produit.model';
 })
 export class UpdateProduitComponent implements OnInit {
   currentProduit  = new Produit();
+  categories : Categorie[];
+  updatedCategorie : Categorie;
+
  
   constructor(private activatedRoute: ActivatedRoute,
               private router :Router,
@@ -18,14 +22,15 @@ export class UpdateProduitComponent implements OnInit {
               }
 
   ngOnInit() {
- 
-  // console.log(this.route.snapshot.params.id);
+   this.categories = this.produitService.listeCategories();
    this.currentProduit = this.produitService.consulterProduit(this.activatedRoute.snapshot.params.id);
    console.log(this.currentProduit);     
   }
 
   updateProduit()
-  { //console.log(this.currentProduit);
+  { 
+    this.updatedCategorie = this.produitService.consulterCategorie(this.currentProduit.categorie.idCat);
+    this.currentProduit.categorie = this.updatedCategorie; 
     this.produitService.updateProduit(this.currentProduit);
     this.router.navigate(['produits']);
    }
