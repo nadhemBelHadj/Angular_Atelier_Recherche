189b56796683fd5b655f004ebef47c52014b161c
Nadhem Bel Hadj <nadhemb@yahoo.com>
2021-12-08 09:11:48 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProduitsComponent } from './produits/produits.component';
 import { AddProduitComponent } from './add-produit/add-produit.component';
 import { UpdateProduitComponent } from './update-produit/update-produit.component';
+import { RechercheParCategorieComponent } from './recherche-par-categorie/recherche-par-categorie.component';
 
 const routes: Routes = [
   {path: "produits", component : ProduitsComponent},
   {path: "add-produit", component : AddProduitComponent},
   {path: "updateProduit/:id",  component: UpdateProduitComponent},
-  { path: "", redirectTo: "produits", pathMatch: "full" }
+  {path: "rechercheParCategorie", component : RechercheParCategorieComponent},
+  {path: "", redirectTo: "produits", pathMatch: "full" }
 ];
 
 @NgModule({
